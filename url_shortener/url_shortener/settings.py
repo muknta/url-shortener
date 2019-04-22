56ebf34a9d13d44a9e4817d5106299d875285953
Nikita <hekntsnv@gmail.com>
2019-04-22 10:52:08 +0300

    changed sqlite to postgres

--- url_shortener/url_shortener/settings.py
+++ url_shortener/url_shortener/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'url_shortener.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_url_shortener',
+        'HOST': 'localhost',
+        'USER': 'url_user',
+        'PASSWORD': 'postgresfordjango',
     }
 }
 
