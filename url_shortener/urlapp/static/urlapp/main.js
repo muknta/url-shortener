25b2811e0df0817bd00fd4b5b8523745f6cdb72c
Nikita <hekntsnv@gmail.com>
2019-05-04 21:17:28 +0300

    add div.login-alert, mod isUrl() in main.js, rm whitenoise in requirements, chngd btns, .gitignore

--- url_shortener/urlapp/static/urlapp/main.js
+++ url_shortener/urlapp/static/urlapp/main.js
@@ -29,14 +29,13 @@ $(function() {
 function returnSuccess(data, textStatus, jqXHR) {
   if(data.url) {
     $('#result').text(data.url);
-    $('#given-url').val("");
   } else {
-    $('#result').text("Do not submit blank.");
+    $('#result').text("Oops..some_error");
   }
 };
 
-function isUrl(s) {
-  var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
-  return regexp.test(s);
+function isUrl(value) {
+  var regexp = /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+  return regexp.test(value);
 };
 
