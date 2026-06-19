b156d3a11f173a40a5dcbb73a879ce6834e59720
Mykyta Sadchenko <31825654+muknta@users.noreply.github.com>
2026-06-19 22:48:18 +0300

    i18n: register i18n plugin in main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router.js";
+import i18n from "./i18n.js";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(i18n).use(router).mount("#app");
